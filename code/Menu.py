c02f435794cdc9cc3f4a4de71ae5854e1a3ec3dc
tayAmorim <amorim.tayanna@gmail.com>
2025-03-27 08:17:38 -0300

    feat: menu movement

--- code/Menu.py
+++ code/Menu.py
@@ -16,21 +16,39 @@ class Menu:
         menu_option = 0
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
+
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.write_text(45, 'JumpQuest', C_GREEN_DARK, ((W_WIDTH / 2), 70))
+            self.write_text(45, 'JumpQuest', C_GREEN_DARK, ((W_WIDTH / 2), 100))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.write_text(25, MENU_OPTION[i], C_RED, ((W_WIDTH / 2), 200 + 25 * i))
+                    self.write_text(25, MENU_OPTION[i], C_RED, ((W_WIDTH / 2), 200 + 20 * i))
                 else:
-                    self.write_text(25, MENU_OPTION[i], C_WHITE_BLUE, ((W_WIDTH / 2), 200 + 25 * i))
+                    self.write_text(25, MENU_OPTION[i], C_WHITE_BLUE, ((W_WIDTH / 2), 200 + 20 * i))
             pygame.display.flip()
 
             for event in pygame.event.get():
+
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.KEYUP:
+                        if event.key == pygame.K_UP:
+                            if menu_option > 0:
+                                menu_option -= 1
+                            else:
+                                menu_option = len(MENU_OPTION) - 1
+
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
 
 
     def write_text(self, text_size: int, text:str, text_color: tuple, text_center_pos: tuple):
